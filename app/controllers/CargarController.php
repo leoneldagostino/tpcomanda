dd77d8a8ef64dda33645aacbe331899dcdddcd76
leoneldagostino <leeonel_@live.com>
2024-07-02 01:56:47 -0300

    sprint3+

---
+++ app/controllers/CargarController.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once './models/Usuario.php';
+require_once './models/Mesa.php';
+require_once './models/Pedido.php';
+require_once './models/Producto.php';
+
+
+class CargarController
+{
+    public static function cargarUsuarios($request, $response, $args)
+    {
+
+    }
+}
